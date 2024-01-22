b3f31f5c745dce30bad6ef25422a9ef154ab07f0
iliadavitashvili <81050721+iliadavitashvili@users.noreply.github.com>
2024-01-22 16:17:28 +0400

    side menu added responsive image fixed

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let screenWidth =
   document.documentElement.clientWidth ||
   document.body.clientWidth;
 
+  
 let mainImg = document.getElementById("main-image");
 const smallImg =
   "https://static.wixstatic.com/media/93e8a3_9fa23196ddf6427a9da574e2b31a13d2~mv2.jpg/v1/fill/w_619,h_420,al_c,q_80,enc_auto/93e8a3_9fa23196ddf6427a9da574e2b31a13d2~mv2.jpg";
@@ -12,20 +13,70 @@ const largeImg =
   "https://static.wixstatic.com/media/93e8a3_9fa23196ddf6427a9da574e2b31a13d2~mv2.jpg/v1/fill/w_1920,h_166,al_c,q_80,enc_auto/93e8a3_9fa23196ddf6427a9da574e2b31a13d2~mv2.jpg";
 
 
+
+function updateImage() {
+  let screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  if (screenWidth < 600) {
+    mainImg.src = smallImg;
+    mainImg.style.height = "317px";
+  } else if (screenWidth < 2300) {
+    mainImg.src = mediumImg;
+    mainImg.style.height = "352px";
+  } else {
+    mainImg.src = largeImg;
+    mainImg.style.height = ""; 
+  }
+}
+
+
+updateImage();
+
+
+window.addEventListener("resize", updateImage);
+
 let coursesDiv = document.getElementById("courses-div")
 
+const bgWrapper = document.getElementById("bg-wrapper")
+
+const menuButton = document.getElementById("menu-button")
+
+const mbFirstLine = document.getElementById("line-1")
+const mbSecondLine = document.getElementById("line-2")
+const mbThirdLine = document.getElementById("line-3")
+const asideMenu = document.getElementById("aside-menu")
+let isMenuClicked = false
+
+const lines =document.querySelectorAll(".line") 
+
+menuButton.addEventListener("click",()=>{
+  if(!isMenuClicked){
+    document.body.style.overflow = (asideMenu.style.display === "none") ? "auto" : "hidden";
+    bgWrapper.style.opacity="0.5"
+    asideMenu.style.transform="translateX(0)"
+    mbFirstLine.style.transform= "rotate(90deg) translateX(2px) translateY(-6px)";
+  menuButton.style.transform ="rotate(-45deg)"
+  mbThirdLine.style.transform= "rotate(-90deg) translateX(2px) translateY(-6px)";
+  lines.forEach((item)=>item.style.backgroundColor ="gray")
+  }else{
+    asideMenu.style.transform="translateX(70vw)"
+    document.body.style.overflow = (asideMenu.style.display === "none") ? "hidden" : "auto";
+
+    bgWrapper.style.opacity="1"
+    lines.forEach(item => item.style.backgroundColor = "")
+    mbFirstLine.style.transform= "";
+  menuButton.style.transform =""
+  mbThirdLine.style.transform= "";
+  }
+  isMenuClicked = !isMenuClicked
+})
 
 
 
-if (screenWidth < 600) {
-  mainImg.src = smallImg;
-  mainImg.style.height = " 317px";
-} else if (screenWidth < 2300) {
-  mainImg.src = mediumImg;
-  mainImg.style.height = "352px";
-} else {
-  mainImg.src = largeImg;
-}
+
 
 const coursesArray = [
   {
@@ -148,23 +199,3 @@ const inner = document.querySelector('.carousel-inner');
 
     resetAutoAdvanceTimer();
 
-{/* <div class="course">
-            <img
-              src="https://static.wixstatic.com/media/dd97f4_6f93a2a737b14a32ae26b17f51e6dbf1~mv2.jpg/v1/fill/w_300,h_150,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_6f93a2a737b14a32ae26b17f51e6dbf1~mv2.jpg"
-            />
-            <div class="course-info-wrapper">
-
-              <div class="course-name-wrapper">
-                <h3>iOS Development</h3>
-                <p>რეგისტრაცია დასრულებულია</p>
-              </div>
-              <div class="course-details-wrapper">
-                <svg preserveAspectRatio="xMidYMid meet" data-bbox="1.833 2.667 13.334 10.666" xmlns="http://www.w3.org/2000/svg" viewBox="1.833 2.667 13.334 10.666" height="16" width="17" data-type="color" role="presentation" aria-hidden="true"><defs><style>#comp-lnoncoa62__12a0b52d-d2f4-498f-8668-ef4342a659a0 svg [data-color="1"] {fill: #00AEEF;}</style></defs>
-                  <g>
-                    <path fill="#00adee" d="m10.3 2.867 4.667 4.666a.645.645 0 0 1 0 .934L10.3 13.133a.644.644 0 0 1-.933 0 .644.644 0 0 1 0-.933L12.9 8.667H2.5c-.4 0-.667-.267-.667-.667s.267-.667.667-.667h10.4L9.367 3.8a.605.605 0 0 1-.2-.467c0-.2.066-.333.2-.466a.644.644 0 0 1 .933 0Z" data-color="1"></path>
-                  </g>
-                </svg>
-                <h3>კურსის დეტალები</h3>
-              </div>
-            </div>
-          </div> */}
